7e57c9f7f839f1f456f28353a714856cd5fee5d8
Y <ym.mail@qq.com>
2019-03-10 20:42:40 +0800

    添加了线性表的添加、删除、查找方法，完成了线性表的基本功能

--- linear_list/linear_list.c
+++ linear_list/linear_list.c
@@ -4,7 +4,10 @@ int main(int argc, char const* argv[])
 {
     linear_list test_list;
     InitLinearList(&test_list);
-    ShowElem(&test_list);
+    for (int i = 0; i < INIT_CAPACITY; i++)
+        Insert(rand() % 100, i, &test_list);
+    qsort(test_list.elem, test_list.length, sizeof(int), Cmp);
+    Display(&test_list);
 
     system("pause");
     return 0;
@@ -15,13 +18,78 @@ linear_list* InitLinearList(linear_list* L)
     L->capacity = INIT_CAPACITY;
     L->length = 0;
     L->elem = (int*)malloc(L->capacity * sizeof(int));
-    for (int i = 0; i < L->capacity; i++)
-        *(L->elem + i) = 0;
+    memset(L->elem, 0, L->capacity * sizeof(int));
     return L;
 }
 
-void ShowElem(linear_list* L)
+void Display(linear_list* L)
 {
-    for (int i = 0; i < L->capacity; i++)
-        printf("ÏßĞÔ±íµÄµÚ%d¸öÔªËØÎª£º %d\n", i + 1, *(L->elem + i));
+    for (int i = 0; i < L->length; i++)
+        printf("çº¿æ€§è¡¨çš„ç¬¬%dä¸ªå…ƒç´ ä¸ºï¼š %d\n", i + 1, L->elem[i]);
+}
+
+int* AllocListElem(linear_list* L)
+{
+    int* new_list_elem = (int*)malloc(L->capacity * sizeof(int));
+    memset(new_list_elem, 0, L->capacity * sizeof(int));
+    memcpy(new_list_elem, L->elem, L->length * sizeof(int));
+    return new_list_elem;
+}
+
+linear_list* Resize(linear_list* L)
+{
+    if (L->length < 0.25 * L->capacity && L->length > 0) {
+        L->capacity *= 0.5;
+        int* new_elem = AllocListElem(L);
+        free(L->elem);
+        L->elem = new_elem;
+        return L;
+    }
+
+    if (L->length > 0.75 * L->capacity && L->length > 0) {
+        L->capacity *= 2;
+        int* new_elem = AllocListElem(L);
+        free(L->elem);
+        L->elem = new_elem;
+        return L;
+    }
+}
+
+int Insert(int E, int P, linear_list* L)
+{
+    Resize(L);
+    for (int i = L->length; i >= P; i--) {
+        L->elem[i] = L->elem[i - 1];
+        L->elem[P] = E;
+    }
+    L->length++;
+    return E;
+}
+
+int Remove(int lo, int hi, linear_list* L)
+{
+    if (lo < 0 || hi > L->length || lo > L->length)
+        printf("åŒºé—´èŒƒå›´é”™è¯¯ï¼\n");
+
+    while (hi < L->length)
+        L->elem[lo++] = L->elem[hi++];
+    L->length -= hi - lo;
+    Resize(L);
+}
+
+int BinSearch(int E, int lo, int hi, linear_list* L)
+{
+    while (lo < hi) {
+        int mid = lo + (hi - lo) / 2;
+        if (L->elem[mid] < E)
+            lo = mid + 1;
+        else
+            hi = mid;
+    }
+    return hi;
+}
+
+int Cmp(const void* a, const void* b)
+{
+    return *(int*)a - *(int*)b;
 }
--- linear_list/linear_list.h
+++ linear_list/linear_list.h
@@ -2,8 +2,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define INIT_CAPACITY 100
+#define INIT_CAPACITY 100 // 设置初始容量为100
 
 typedef struct
 {
@@ -12,5 +13,74 @@ typedef struct
     int* elem;
 } linear_list;
 
+/**
+ * @brief 初始化线性表L，将其长度设置为0，容量设置为初始容量，元素全部初始化为0
+ * 
+ * @param L 线性表指针
+ * @return linear_list* 初始化完成的线性表指针
+ */
 linear_list* InitLinearList(linear_list* L);
-void ShowElem(linear_list *L);
+
+/**
+ * @brief 输出线性表L的所有元素
+ * 
+ * @param L 线性表指针
+ */
+void Display(linear_list* L);
+
+/**
+ * @brief 为线性表L的元素重新分配内存空间
+ * 
+ * @param L 线性表指针
+ * @return int* 分配好空间并初始化的线性表元素指针
+ */
+int* AllocListElem(linear_list* L);
+
+/**
+ * @brief 当线性表L的长度大于3/4容量或者小于1/4容量时重新分配线性表的容量
+ * 
+ * @param L 线性表指针 
+ * @return linear_list* 重新分配容量的线性表指针
+ */
+linear_list* Resize(linear_list* L);
+
+/**
+ * @brief 在线性表L的P位置插入元素E
+ * 
+ * @param E 需要插入的元素E
+ * @param P 元素插入的位置P
+ * @param L 将要被插入元素E的线性表L
+ * @return int 被插入的元素
+ */
+int Insert(int E, int P, linear_list* L);
+
+/**
+ * @brief 删除线性表中范围为(lo,hi]的元素
+ * 
+ * @param lo 线性表中的较低元素位置
+ * @param hi 线性表中的较高元素位置
+ * @param L 将要被删除元素的线性表的指针
+ * @return int 被删除的元素值
+ */
+int Remove(int lo, int hi, linear_list* L);
+
+/**
+ * @brief 二分查找[lo,hi)区间内的元素E，如果没有找到则返回最初区间的最后一个值。使用前需要将线性表升序排列
+ * 
+ * @param E 需要查找的元素
+ * @param lo 线性表中较低元素的位置
+ * @param hi 线性表中较高元素的位置
+ * @param L 需要被查找的线性表指针L
+ * @return int 需要查找元素的位置
+ */
+int BinSearch(int E, int lo, int hi, linear_list* L);
+
+/**
+ * @brief 比较a，b两个值的大小，并根据大于、等于、小于分别返回大于0，等于0，小于0
+ * 本函数用于qsort()函数的使用
+ * 
+ * @param a 比较元素a
+ * @param b 比较元素b
+ * @return int 比较结果
+ */
+int Cmp(const void* a,const void *b);
